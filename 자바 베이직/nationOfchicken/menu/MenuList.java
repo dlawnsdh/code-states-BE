0de274877a8e59995f48e5a07020b4b25b3e640e
dlawnsdh <77203609+dlawnsdh@users.noreply.github.com>
2022-09-08 17:57:54 +0900

    Create MenuList.java

---
+++ 자바 베이직/nationOfchicken/menu/MenuList.java
@@ -0,0 +1,12 @@
+package nationOfchicken;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuList {
+    private List<Menu> menuList = new ArrayList<>();
+
+    public List<Menu> getMenuList() {
+        return menuList;
+    }
+}
